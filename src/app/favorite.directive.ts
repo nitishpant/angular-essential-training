5a3fc4fcddcdf6b7b66d78255d709089cf613146
Nitish Pant <nitishpant@users.noreply.github.com>
2021-08-15 14:22:56 +0530

    added custom declarative

---
+++ src/app/favorite.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostBinding } from "@angular/core";
+
+@Directive({
+    selector: '[mwFavorite]'
+})
+
+export class FavoriteDirective{
+    @HostBinding('class.is-favorite') isFavorite=true;
+}
+
+
+
+
+
